a4ee6996f9a4ad6ca1081ad1482a654ccd4c9a9c
rua-iri <rory.mcguigan@protonmail.com>
2024-05-02 16:38:01 +0300

    update: give settings component basic outline

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -3,8 +3,33 @@
 export default function Settings() {
 
     return (
-        <div className="">
-            
-        </div>
+        <div className="absolute z-20 top-0 w-screen h-screen backdrop-blur-md">
+                <div className="flex h-screen">
+                    <div className="m-auto">
+                        <div className='flex justify-center my-20 bg-white'>
+                            <div className='flex-col items-center justify-center border border-slate-300 rounded-md'>
+                                <div className="flex justify-center mb-12 border-b border-slate-300 bg-limeGreenpx-20 py-5">
+                                    Game Over
+                                </div>
+
+                                <div className="px-20 pb-10">
+                                    <div className="flex-none">
+                                        <div className="mb-10">
+                                            You scored: 
+                                        </div>
+                                        <div>
+                                            <button
+                                                className="py-2 px-4 bg-softOrange rounded-md border border-slate-300"
+                                                onClick={() => alert("Close Settings")}>
+                                                Close
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
     )
 }
